76721a014cd3934df3cca3e7a4ca11f2c014f462
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-03-30 19:55:38 -0300

    Rubocop issues fix

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,7 +1,7 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[show edit update destroy]
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: %i[index show]
+  before_action :correct_user, only: %i[edit update destroy]
   # GET /friends or /friends.json
   def index
     @friends = Friend.all
@@ -13,7 +13,7 @@ class FriendsController < ApplicationController
   # GET /friends/new
   def new
     @friend = current_user.friends.build
-    #@friend = Friend.new
+    # @friend = Friend.new
   end
 
   # GET /friends/1/edit
@@ -21,7 +21,7 @@ class FriendsController < ApplicationController
 
   # POST /friends or /friends.json
   def create
-    #@friend = Friend.new(friend_params)
+    # @friend = Friend.new(friend_params)
     @friend = current_user.friends.build(friend_params)
 
     respond_to do |format|
@@ -58,9 +58,9 @@ class FriendsController < ApplicationController
     end
   end
 
-  def correct_user 
+  def correct_user
     @friend = current_user.friends.find_by(id: params[:id])
-    redirect_to friends_path, notice: "Not Authorized!" if @friend.nill?
+    redirect_to friends_path, notice: 'Not Authorized!' if @friend.nill?
   end
 
   private
